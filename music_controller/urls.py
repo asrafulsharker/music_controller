6f21f6bd148bf2e6348deb702f616a856a097dee
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-07-12 20:14:46 +0600

    add

---
+++ music_controller/urls.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from api import urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+]
