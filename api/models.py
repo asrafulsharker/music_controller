fbd0f253fe10a7309712c9a0ef61169eb9c1916b
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-07-13 12:54:00 +0600

    add

--- api/models.py
+++ api/models.py
@@ -19,6 +19,3 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-    
